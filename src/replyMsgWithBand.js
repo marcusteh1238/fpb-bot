652111dba14498b92cae77b3eb72b881276f20aa
Teh Hao Rui Marcus <50549441+marcusteh1238@users.noreply.github.com>
2021-11-22 20:03:01 +0800

    use gif attachment as reply

--- src/replyMsgWithBand.js
+++ src/replyMsgWithBand.js
@@ -1,4 +1,8 @@
+const fs = require("fs");
+
 const { Message } = require("discord.js");
+const path = require("path");
+const bandGif = fs.readFileSync(path.resolve(__dirname, "./data/files/lucky.gif"));
 
 /**
  * 
@@ -7,7 +11,12 @@ const { Message } = require("discord.js");
  */
 async function replyMsgWithBand(message) {
   if (!message.content || !message.content.startsWith('!band')) return;
-  message.reply("https://media.discordapp.net/attachments/892675220593324072/912303927536541696/lucky.gif");
+  // message.reply("https://media.discordapp.net/attachments/892675220593324072/912303927536541696/lucky.gif");
+  message.reply({
+    files: [
+      {attachment: bandGif, name: "band.gif"}
+    ]
+  });
 }
 
 module.exports = replyMsgWithBand;
